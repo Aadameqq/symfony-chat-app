93ae731296f40b489cc33374b4a7b436552ffeb3
Aadameqq <adamek5599.sluzbowy@gmail.com>
2024-03-15 16:56:00 +0100

    feat: account creation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,45 @@
 
 namespace App\Controller;
 
+use App\Dto\CreateUserDto;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/user')]
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): JsonResponse
+
+
+    public function __construct(private readonly UserPasswordHasherInterface $passwordHasher, private readonly EntityManagerInterface $entityManager, private readonly UserRepository $userRepo)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+    }
+
+    #[Route('', methods: ["POST"])]
+    public function index(#[MapRequestPayload] CreateUserDto $dto): JsonResponse
+    {
+
+        $found = $this->userRepo->findOneBy(['username' => $dto->username]);
+
+        if($found){
+            throw new ConflictHttpException('User with given username already exists');
+        }
+
+        $user = new User();
+        $user->setUsername($dto->username);
+        $hashedPassword = $this->passwordHasher->hashPassword($user,$dto->plainPassword);
+        $user->setPassword($hashedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $this->json(['message' => "Account Created"], Response::HTTP_CREATED);
     }
 }
---
+++ src/Dto/CreateUserDto.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Dto;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class CreateUserDto
+{
+
+    public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 3, max: 100)]
+        public readonly string $username,
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 12)]
+        #[\SensitiveParameter]
+        //TODO: add more validation
+        public readonly string $plainPassword
+    )
+    {
+
+    }
+}
